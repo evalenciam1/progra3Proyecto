27b09eb951344fff9cf16e1d362dc2a78fd6baa7
evalenciam1 <evalenciam1@miumg.edu.gt>
2025-05-10 15:15:10 -0600

    Update

--- TransporteNavex/src/main/java/com/mycompany/transportenavex/TransporteNavex.java
+++ TransporteNavex/src/main/java/com/mycompany/transportenavex/TransporteNavex.java
@@ -9,8 +9,7 @@ package com.mycompany.transportenavex;
  * @author Esteban Valencia
  */
 public class TransporteNavex {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
     }
 }
