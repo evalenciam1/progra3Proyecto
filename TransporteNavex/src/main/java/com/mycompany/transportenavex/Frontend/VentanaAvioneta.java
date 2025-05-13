a54c329c61bb63db8034bed7fb296255bd6a8ad3
evalenciam1 <evalenciam1@miumg.edu.gt>
2025-05-12 23:29:27 -0600

    Complemento Frontend
    Actualizacion frontend

--- TransporteNavex/src/main/java/com/mycompany/transportenavex/Frontend/VentanaAvioneta.java
+++ TransporteNavex/src/main/java/com/mycompany/transportenavex/Frontend/VentanaAvioneta.java
@@ -14,9 +14,6 @@ public class VentanaAvioneta extends JFrame {
     private ListaDoblementeEnlazada listaPasajeros;
 
     // Elementos visuales
-    private JTextField dpiField;
-    private JTextField nombreField;
-    private JTextField asientoField;
     private JTextArea listaTextArea;
 
     public VentanaAvioneta(int numeroavioneta, ListaDoblementeEnlazada listaPasajeros) {
@@ -39,27 +36,10 @@ public class VentanaAvioneta extends JFrame {
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
-        // Campos de entrada
-        dpiField = new JTextField(15);
-        nombreField = new JTextField(15);
-        asientoField = new JTextField(5);
-
-        // Agregando cuadro de entrada DPI
-        panel.add(new JLabel("DPI:"));
-        panel.add(dpiField);
-        panel.add(Box.createVerticalStrut(10));
-        // Agregando campo de entrada Nombre
-        panel.add(new JLabel("Nombre:"));
-        panel.add(nombreField);
-        panel.add(Box.createVerticalStrut(10));
-        // Agregando campo de entrada asiento
-        panel.add(new JLabel("No. de asiento:"));
-        panel.add(asientoField);
-        panel.add(Box.createVerticalStrut(10));
 
         // Botón agregar
         JButton agregarButton = new JButton("Agregar pasajero");
-        agregarButton.addActionListener(e -> agregarPasajero());
+        agregarButton.addActionListener(e -> ventanaAgregarPasajero());
         panel.add(agregarButton);
 
         panel.add(Box.createVerticalStrut(15));
@@ -76,30 +56,8 @@ public class VentanaAvioneta extends JFrame {
         actualizarLista();
     }
 
-    private void agregarPasajero() {
-        String dpi = dpiField.getText().trim();
-        String nombre = nombreField.getText().trim();
-        int asiento;
-
-        try {
-            asiento = Integer.parseInt(asientoField.getText().trim());
-        } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(this, "Número de asiento inválido.");
-            return;
-        }
-
-        Pasajero nuevo = new Pasajero(dpi, nombre, asiento);
-        String respuesta = AccionesController.agregarPasajero(listaPasajeros, nuevo);
-        JOptionPane.showMessageDialog(this, respuesta);
-
-        if (respuesta=="PASAJERO AGREGADO CORRECTAMENTE") {
-            dpiField.setText("");
-            nombreField.setText("");
-            asientoField.setText(""); 
-        }
-        
-
-        actualizarLista();
+    private void ventanaAgregarPasajero() {
+         new agregarPasajero(numeroAvioneta, listaPasajeros);
     }
 
     private void actualizarLista() {
